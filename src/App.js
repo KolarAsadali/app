bd87fe802ac3b1c81acd08caa9dea062afb25ddf
KolarAsadali <106642940+KolarAsadali@users.noreply.github.com>
2022-12-06 18:56:51 +0530

    add files as upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Category from './Components/Category';
+import Food from './Components/Food';
+import HeadlineCards from './Components/Headlinecards';
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <HeadlineCards />
+        <Food />
+        <Category />
+    </div>
+  );
+}
+
+export default App;
